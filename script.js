d24043365ee85fbcdbfb18e9a9d9aa7ae451956b
Santu De <62021656+santude1997@users.noreply.github.com>
2021-01-30 22:41:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2; //hour rotation
+    mrotation = 6*mtime;            //minit rotation
+    srotation = 6*stime;            //second rotation
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
